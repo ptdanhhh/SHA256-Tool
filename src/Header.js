88b9e68361556627af6a502b9490b22d12fb3cc3
emthangtrung <danhpham612@gmail.com>
2022-11-11 14:45:53 -0800

    test

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+function Header() {
+  return (
+    <section className="header-section">
+      <div>
+        <h1>CS166 Group 10</h1>
+      </div>
+      <div>
+        <h1>SHA-256 Secure Hashing Algorithm Tool</h1>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
---
+++ src/InputField.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './index.css';
+
+
+function InputField() {
+  return (
+    <section>
+      <div className='wrapperInput'>
+        <div className='hash-content'>
+          <div className='hash-in'>
+
+
+            <h2>Input string here</h2>
+            <div className='text-area'>
+              <textarea className='textarea' placeholder='Type here...' required></textarea>
+            </div>
+
+            <div className='btn-in'>
+
+              <div className='hashbtn'>
+                <button className="btn btn--hash">Hash</button>
+              </div>
+
+              <div className='clearbtn'>
+                <button className="btn btn--clear">Clear</button>
+              </div>
+          
+            </div>
+
+          </div>
+
+
+          <div className='hash-out'>
+            <h2>SHA-256 Output:</h2>
+            
+          </div>
+        </div>
+
+        {/* end hash-content */}
+        <div className='sha-text'>
+            <div className='sha-title'>
+              <h2>what is SHA-256?</h2>
+            </div>
+        </div>
+
+
+
+      </div>  
+    </section>
+  )
+}
+
+export default InputField
